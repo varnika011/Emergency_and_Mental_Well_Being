56208be61fabf76403557392154af3b8e5817b30
SSJB-GOD <sonuborkarbcca2001@gmail.com>
2024-10-16 19:27:23 +0530

    Initial commit with folder

---
+++ backend/Emergency_and_Mental_WellBeing/src/main/java/com/infosys/Emergency_and_Mental_WellBeing/EmergencyAndMentalWellBeingApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.Emergency_and_Mental_WellBeing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyAndMentalWellBeingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyAndMentalWellBeingApplication.class, args);
+	}
+
+}
